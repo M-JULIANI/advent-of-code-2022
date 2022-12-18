8b89cbba587c950c2113de69daff1b86f82fa506
Marco Juliani <marco@outerlabs.io>
2022-12-18 00:05:57 +0000

    Day14 pt2

--- objects/cell.ts
+++ objects/cell.ts
@@ -40,4 +40,43 @@ export class Cell{
         return null;
     }
 
+    getMove2(map: Map<string, Cell>, maxHeight: number): Cell | null | undefined{
+
+        const left = [-1, 1];
+        const bottom = [0, 1];
+        const right = [1, 1];
+
+        const movement = [bottom, left, right];
+
+        let allNeighborsInFirstRow = 0;
+        let allThreeOcupied = 0;
+        for(let i =0; i<3; i++){
+            const possibleNeighbor = {x: this.pos.x + movement[i][0], y: this.pos.y + movement[i][1]} as Vec2
+            if(possibleNeighbor.y ===1) allNeighborsInFirstRow++;
+            const neighborInMap = map.get(vecToString(possibleNeighbor))
+            if(neighborInMap !=null){
+                if(neighborInMap.occupied) allThreeOcupied++;
+            }
+        }
+        if(allNeighborsInFirstRow && allThreeOcupied ===3) return undefined; //return if stuck at the beginning
+
+        //otherwise proceed as normal
+        for(let i =0; i<3; i++){
+            const possibleNeighbor = {x: this.pos.x + movement[i][0], y: this.pos.y + movement[i][1]} as Vec2
+            const neighborInMap = map.get(vecToString(possibleNeighbor))
+            //if null, add to map, and continue, unless new cell is unoccupied, else return it
+            if(neighborInMap == null){
+                const occupied = possibleNeighbor.y === maxHeight-2 ? true : false;
+                const newCell = new Cell(possibleNeighbor.x, possibleNeighbor.y, occupied)
+                map.set(vecToString(newCell.pos), newCell);
+                if(!occupied) return newCell;
+                continue;
+            }
+            if(neighborInMap!.occupied) continue;
+            return neighborInMap;
+        }
+
+        return null;
+    }
+
 }
--- problems/solvers/day14.ts
+++ problems/solvers/day14.ts
@@ -5,6 +5,52 @@ import { Vec2, vecToString } from '../../objects/objects';
 import { map_range} from '../../helpers/mapNumbers'
 
 
+const redraw = (matrix: number[][], map: Map<string, Cell>)=>{
+    for(let x = 0; x< matrix.length; x++){
+        for(let y= 0; y< matrix[0].length; y++){
+            let vec = {x: y, y: x} as Vec2;
+            let exists = map.get(vecToString(vec))
+            if(exists == null) continue;
+            matrix[x][y] = exists.occupied ? 1: 0;
+        }
+      }
+      console.log(matrix);
+  }
+
+    const moveSand = (s: Cell, map: Map<string, Cell>): boolean => {
+
+        let currentCell: Cell | null | undefined = new Cell(s.i, s.j, s.occupied);
+        let lastCurrent = currentCell;
+        while (true) {
+            currentCell = currentCell.getMove(map)
+            if (currentCell === undefined) return true; //if it went out of bounds
+            if (currentCell == null) break; //if cannot move any more
+            lastCurrent = currentCell;
+        }
+
+        lastCurrent!.flip();
+        map.set(vecToString(lastCurrent!.pos), lastCurrent!)
+
+        return false;
+    }
+
+    const moveSand2 = (s: Cell, map: Map<string, Cell>, maxVert: number): boolean => {
+
+        let currentCell: Cell | null | undefined = new Cell(s.i, s.j, s.occupied);
+        let lastCurrent = currentCell;
+        while (true) {
+            if(currentCell.pos.y === maxVert) return true; //if it got stuck
+            currentCell = currentCell.getMove2(map, maxVert)
+            if (currentCell === undefined) return true; //if it got stuck
+            if (currentCell == null) break; //if cannot move any more
+            lastCurrent = currentCell;
+        }
+
+        lastCurrent!.flip();
+        map.set(vecToString(lastCurrent!.pos), lastCurrent!)
+
+        return false;
+    }
 
 export const pt1 = (f: string)=>{
 
@@ -45,31 +91,19 @@ export const pt1 = (f: string)=>{
             let yNext = path[p+1][1]
 
             if (xNext === xCurrent) {
-                if (yCurrent < yNext) {
-                    for (let y = yCurrent; y < yNext; y++) {
-                        if (yCurrent < minY) minY = yCurrent;
-                        if (yCurrent > maxY) maxY = yCurrent;
-                    }
-                }
-                else{
-                    for (let y = yCurrent; y > yNext; y--) {
-                        if (yCurrent < minY) minY = yCurrent;
-                        if (yCurrent > maxY) maxY = yCurrent;
-                    }
+                let yMin = yCurrent < yNext ? yCurrent : yNext;
+                let yMax = yCurrent < yNext ? yNext : yCurrent;
+                for (let y = yMin; y < yMax; y++) {
+                    if (yCurrent < minY) minY = yCurrent;
+                    if (yCurrent > maxY) maxY = yCurrent;
                 }
             }
             else{
-                if (xCurrent > xNext) {
-                    for (let x = xCurrent; x > xNext; x--) {
-                        if (xCurrent < minX) minX = xCurrent;
-                        if (xCurrent > maxX) maxX = xCurrent;
-                    }
-                }
-               else{
-                    for (let x = xCurrent; x < xNext; x++) {
-                        if (xCurrent < minX) minX = xCurrent;
-                        if (xCurrent > maxX) maxX = xCurrent;
-                    }
+                let xMin = xCurrent < xNext ? xCurrent : xNext;
+                let xMax = xCurrent < xNext ? xNext : xCurrent;
+                for (let x = xMin; x < xMax; x++) {
+                    if (xCurrent < minX) minX = xCurrent;
+                    if (xCurrent > maxX) maxX = xCurrent;
                 }
             }
         }
@@ -102,31 +136,19 @@ export const pt1 = (f: string)=>{
              yNext = map_range(yNext, minY, maxY, 0, maxY - minY);
  
              if(xNext === xCurrent){
-                if (yCurrent < yNext) {
-                    for (let y = yCurrent; y < yNext; y++) {
-                        const cell = new Cell(xCurrent, y, true)
-                        map.set(vecToString(cell.pos), cell);
-                    }
-                }
-                else{
-                    for (let y = yCurrent; y > yNext; y--) {
-                        const cell = new Cell(xCurrent, y, true)
-                        map.set(vecToString(cell.pos), cell);
-                    }
-                }
+                 let yMin = yCurrent < yNext ? yCurrent : yNext;
+                 let yMax = yCurrent < yNext ? yNext : yCurrent;
+                 for (let y = yMin; y < yMax; y++) {
+                     const cell = new Cell(xCurrent, y, true)
+                     map.set(vecToString(cell.pos), cell);
+                 }
              }
              else {
-                 if (xCurrent > xNext) {
-                     for (let x = xCurrent; x > xNext; x--) {
-                         const cell = new Cell(x, yCurrent, true)
-                         map.set(vecToString(cell.pos), cell);
-                     }
-                 }
-                 else {
-                     for (let x = xCurrent; x < xNext; x++) {
-                        const cell = new Cell(x, yCurrent, true)
-                        map.set(vecToString(cell.pos), cell);
-                     }
+                 let xMin = xCurrent < xNext ? xCurrent : xNext;
+                 let xMax = xCurrent < xNext ? xNext : xCurrent;
+                 for (let x = xMin; x < xMax; x++) {
+                     const cell = new Cell(x, yCurrent, true)
+                     map.set(vecToString(cell.pos), cell);
                  }
              }
          }
@@ -147,35 +169,6 @@ export const pt1 = (f: string)=>{
     }
   }
 
-  const redraw = (matrix: number[][], map: Map<string, Cell>)=>{
-    for(let x = 0; x< matrix.length; x++){
-        for(let y= 0; y< matrix[0].length; y++){
-            let vec = {x: y, y: x} as Vec2;
-            let exists = map.get(vecToString(vec))
-            if(exists == null) continue;
-            matrix[x][y] = exists.occupied ? 1: 0;
-        }
-      }
-      console.log(matrix);
-  }
-
-    const moveSand = (s: Cell, map: Map<string, Cell>): boolean => {
-
-        let currentCell: Cell | null | undefined = new Cell(s.i, s.j, s.occupied);
-        let lastCurrent = currentCell;
-        while (true) {
-            currentCell = currentCell.getMove(map)
-            if(currentCell === undefined) return true; //if it went out of bounds
-            if (currentCell == null) break; //if cannot move any more
-            lastCurrent = currentCell;
-        }
-
-        lastCurrent!.flip();
-        map.set(vecToString(lastCurrent!.pos), lastCurrent!)
-
-        return false;
-    }
-
     start = {
         x: map_range(start.x, minX, maxX, 0, maxX - minX),
         y: map_range(start.y, minY, maxY, 0, maxY - minY)
@@ -189,5 +182,151 @@ export const pt1 = (f: string)=>{
 
 redraw(matrix, map);
 console.log('iteration: ' + iteration)
+
+}
+
+export const pt2 = (f: string)=>{
+
+    const rockPackInput = fs.readFileSync(f, { encoding: 'utf-8' })
+    .trim()
+    .split("\n");
+
+    const rockPaths = rockPackInput.map(x=> {
+        const items = x.split('->');
+        const pairs = items.map(y=> {
+            const xy = y.split(',');
+            return [Number(xy[0]), Number(xy[1])]
+        })
+        return pairs;
+    });
+
+    const map = new Map<string, Cell>();
+
+    let minX = 100000;
+    let maxX = 0;
+    let minY = 100000;
+    let maxY = 0;
+
+    //get min/max
+    rockPaths.forEach(path=>{
+        for(let p = 0; p<path.length; p++){
+            let xCurrent = path[p][0]
+            let yCurrent = path[p][1]
+
+            if(xCurrent< minX) minX = xCurrent;
+            if(xCurrent> maxX) maxX = xCurrent;
+            if(yCurrent< minY) minY = yCurrent;
+            if(yCurrent> maxY) maxY = yCurrent;
+
+            if(p === path.length-1) return;
+
+            let xNext = path[p+1][0]
+            let yNext = path[p+1][1]
+
+            if (xNext === xCurrent) {
+                let yMin = yCurrent < yNext ? yCurrent : yNext;
+                let yMax = yCurrent < yNext ? yNext : yCurrent;
+                for (let y = yMin; y < yMax; y++) {
+                    if (yCurrent < minY) minY = yCurrent;
+                    if (yCurrent > maxY) maxY = yCurrent;
+                }
+            }
+            else{
+                let xMin = xCurrent < xNext ? xCurrent : xNext;
+                let xMax = xCurrent < xNext ? xNext : xCurrent;
+                for (let x = xMin; x < xMax; x++) {
+                    if (xCurrent < minX) minX = xCurrent;
+                    if (xCurrent > maxX) maxX = xCurrent;
+                }
+            }
+        }
+    })
+
+    let start = {x: 500, y: 0} as Vec2;
+    let startCell = new Cell(start.x, start.y, false);
+    map.set(vecToString(startCell.pos), startCell);
+    if(start.x< minX) minX = start.x;
+    if(start.x> maxX) maxX = start.x;
+    if(start.y< minY) minY = start.y;
+    if(start.y> maxY) maxY = start.y;
+
+    maxY +=2;
+
+    //populate the map
+    rockPaths.forEach(path=>{
+         for(let p = 0; p<path.length; p++){
+             let xCurrent = path[p][0]
+             xCurrent = map_range(xCurrent, minX, maxX, 0, maxX - minX);
+             let yCurrent = path[p][1]
+             yCurrent = map_range(yCurrent, minY, maxY, 0, maxY - minY);
+ 
+             const currentCell = new Cell(xCurrent, yCurrent, true);
+             map.set(vecToString(currentCell.pos), currentCell);
+ 
+             if(p === path.length-1) return;
+ 
+             let xNext = path[p+1][0]
+             xNext = map_range(xNext, minX, maxX, 0, maxX - minX);
+             let yNext = path[p+1][1]
+             yNext = map_range(yNext, minY, maxY, 0, maxY - minY);
+ 
+             if(xNext === xCurrent){
+                 let yMin = yCurrent < yNext ? yCurrent : yNext;
+                 let yMax = yCurrent < yNext ? yNext : yCurrent;
+                 for (let y = yMin; y < yMax; y++) {
+                     const cell = new Cell(xCurrent, y, true)
+                     map.set(vecToString(cell.pos), cell);
+                 }
+             }
+             else {
+                 let xMin = xCurrent < xNext ? xCurrent : xNext;
+                 let xMax = xCurrent < xNext ? xNext : xCurrent;
+                 for (let x = xMin; x < xMax; x++) {
+                     const cell = new Cell(x, yCurrent, true)
+                     map.set(vecToString(cell.pos), cell);
+                 }
+             }
+         }
+     })
+
+     const ySpread = maxY-minY;
+     const xSpread = maxX - minX;
+     for(let y = 0; y< ySpread+1; y++){
+        for(let x= 0; x< xSpread+1; x++){
+            if(y === ySpread){
+            const cell = new Cell(x, y, true)
+            map.set(vecToString(cell.pos), cell);
+            }
+        }
+    }
+    
+  let matrix = makeMatrix(xSpread+1, ySpread+1, 0);
+
+  for(let x = 0; x< matrix.length; x++){
+    for(let y= 0; y< matrix[0].length; y++){
+        let vec = {x: y, y: x} as Vec2;
+        let exists = map.get(vecToString(vec))
+        if(exists == null) {
+            const cell = new Cell(y, x, false);
+            map.set(vecToString(cell.pos), cell)
+        }
+        exists = map.get(vecToString(vec))
+        matrix[x][y] = exists!.occupied ? 1: 0;
+    }
+  }
+
+    start = {
+        x: map_range(start.x, minX, maxX, 0, maxX - minX),
+        y: map_range(start.y, minY, maxY, 0, maxY - minY)
+    } as Vec2;
+    startCell = new Cell(start.x, start.y, false);
+   let iteration = 0;
+    while (true) {
+        if(moveSand2(startCell, map, maxY+2))break;
+        iteration++;
+    }
+
+redraw(matrix, map);
+console.log('iteration: ' + (iteration+1))
 
 }
